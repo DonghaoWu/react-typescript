6ebe8ba569841b40e7a379ebb7df49721c6b7545
Donghao Wu <donghao.wu.us@gmail.com>
2021-06-15 23:21:17 -0400

    add notes about redux hooks.

--- app-2-ts/src/Components/RepositoriesList.tsx
+++ app-2-ts/src/Components/RepositoriesList.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import { useTypedSelector } from '../hooks/useTypedSelector';
 import { useActions } from '../hooks/useActions';
+import { useTypedSelector } from '../hooks/useTypedSelector';
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
